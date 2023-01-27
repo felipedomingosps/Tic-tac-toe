aebe0c128e21ade228c23bc4defce5be1be41bdc
Felipe Domingos <felipedomingosps@gmail.com>
2023-01-27 08:24:02 -0300

    Add configureStartScreen()

---
+++ app/main.js
@@ -0,0 +1,26 @@
+
+
+const configureStartScreen = (() => {
+    const form = document.querySelector('#form');
+
+    let playerOneName;
+    let playerTwoName;
+
+    function getPlayersNames() {
+        return [playerOneName, playerTwoName];
+    }
+
+    function submitForm(event) {
+        event.preventDefault();
+        playerOneName = document.querySelector('#player-one-name').value;
+        playerTwoName = document.querySelector('#player-two-name').value;
+
+        document.querySelector('main').innerHTML = '';
+        //Get marks choice.
+    }
+
+    form.addEventListener('submit', submitForm);
+
+
+    return {getPlayersNames};
+})();
