5f249c461f7177547f6d49e30423ac59f8e56d69
Felipe Domingos <felipedomingosps@gmail.com>
2023-01-29 11:42:05 -0300

    Add render function

--- app/main.js
+++ app/main.js
@@ -24,3 +24,28 @@ const configureStartScreen = (() => {
 
     return {getPlayersNames};
 })();
+
+const render = (() => {
+    const main = document.querySelector('main');
+
+
+    const clear = () => {
+        main.innerHTML = '';
+    } 
+    const startScreen = () => {
+        main.innerHTML = 
+    `<form id="form">
+        <div class="player-one">
+            <label for="player-one-name">Player One: </label><input type="text" id="player-one-name" placeholder="Insert name here" required> <span class="x-mark">X</span>
+        </div>
+        
+        <div class="player-two">
+            <label for="player-two-name">Player Two: </label><input type="text" id="player-two-name" placeholder="Insert name here" required> <span class="o-mark">O</span>
+        </div>
+                    
+        <button type="submit" id="play-button">Play</button>
+    </form>`;
+    };
+
+    return {clear, startScreen};
+})();
